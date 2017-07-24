eaa9fdf6d0709c45e05d4093ae41599b9fb48f5f
SURFsara-e-infra <ron.trompert@surfsara.nl>
2017-07-24 17:48:35 +0200

    -H Expect: needed voor lighttpd removed

--- prepare_test.sh
+++ prepare_test.sh
@@ -9,7 +9,7 @@ IF=/dev/zero
 OF=/dev/null
 
 mkdir -p $WRITEDIR
-NUMCONCURRENT=100
+NUMCONCURRENT=10
 output=output${TESTFILE_SIZE_KB}
 error=error${TESTFILE_SIZE_KB}
 
@@ -18,21 +18,21 @@ dd if=$IF of=$WRITEDIR/file${TESTFILE_SIZE_KB} bs=${TESTFILE_SIZE_KB} count=1024
 wait
 
 # If remote directory to read files from already exists, then delete it.
-echo "curl -s -S -k --user $USER:$PASSWD -H "Expect:" -X DELETE -m $TIMEOUT --connect-timeout $TIMEOUT -L ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/readtestfiles${TESTFILE_SIZE_KB}" >>$output
-curl -s -S -k --user $USER:$PASSWD -X DELETE -H "Expect:" -m $TIMEOUT --connect-timeout $TIMEOUT -L ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/readtestfiles${TESTFILE_SIZE_KB} 1>> $output 2>$error
+echo "curl -s -S -k --user $USER:$PASSWD -X DELETE -m $TIMEOUT --connect-timeout $TIMEOUT -L ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/readtestfiles${TESTFILE_SIZE_KB}" >>$output
+curl -s -S -k --user $USER:$PASSWD -X DELETE -m $TIMEOUT --connect-timeout $TIMEOUT -L ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/readtestfiles${TESTFILE_SIZE_KB} 1>> $output 2>$error
 
 # If remote directory to write files to already exists, then delete it.
-echo "curl -s -S -k --user $USER:$PASSWD -X DELETE -H "Expect:" -m $TIMEOUT --connect-timeout $TIMEOUT -L ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/readtestfiles${TESTFILE_SIZE_KB}" >>$output
-curl -s -S -k --user $USER:$PASSWD -X DELETE -H "Expect:" -m $TIMEOUT --connect-timeout $TIMEOUT -L ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/writetestfiles${TESTFILE_SIZE_KB} 1>> $output 2>>$error
+echo "curl -s -S -k --user $USER:$PASSWD -X DELETE -m $TIMEOUT --connect-timeout $TIMEOUT -L ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/readtestfiles${TESTFILE_SIZE_KB}" >>$output
+curl -s -S -k --user $USER:$PASSWD -X DELETE -m $TIMEOUT --connect-timeout $TIMEOUT -L ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/writetestfiles${TESTFILE_SIZE_KB} 1>> $output 2>>$error
 
 
 # Create remote directory to read files from
-echo "curl -s -S -k --user $USER:$PASSWD -H "Expect:" -X MKCOL ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/readtestfiles${TESTFILE_SIZE_KB}" >>$output
-curl -s -S -k --user $USER:$PASSWD -H "Expect:" -X MKCOL ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/readtestfiles${TESTFILE_SIZE_KB} 1>> $output 2>>$error
+echo "curl -s -S -k --user $USER:$PASSWD -X MKCOL ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/readtestfiles${TESTFILE_SIZE_KB}" >>$output
+curl -s -S -k --user $USER:$PASSWD -X MKCOL ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/readtestfiles${TESTFILE_SIZE_KB} 1>> $output 2>>$error
 
 # Create remote directory to write files to
-echo "curl -s -S -k --user $USER:$PASSWD -H "Expect:" -X MKCOL ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/writetestfiles${TESTFILE_SIZE_KB}" >>$output
-curl -s -S -k --user $USER:$PASSWD -H "Expect:" -X MKCOL ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/writetestfiles${TESTFILE_SIZE_KB} 1>> $output 2>>$error
+echo "curl -s -S -k --user $USER:$PASSWD -X MKCOL ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/writetestfiles${TESTFILE_SIZE_KB}" >>$output
+curl -s -S -k --user $USER:$PASSWD -X MKCOL ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/writetestfiles${TESTFILE_SIZE_KB} 1>> $output 2>>$error
 
 
 # Loop over all files that will be created on the remote machine to do the read
@@ -46,8 +46,8 @@ do
     while [ $i -le ${NUMCONCURRENT} -a $k -le $FILES ]
     do
 
-        echo "curl -s -S -k --connect-timeout 10 --user $USER:$PASSWD -H "Expect:" -T $WRITEDIR/file${TESTFILE_SIZE_KB} -L ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/readtestfiles${TESTFILE_SIZE_KB}/testfile_${TESTFILE_SIZE_KB}_$k" >>$output
-        curl -s -S -k --connect-timeout 10 --user $USER:$PASSWD -H "Expect:" -T $WRITEDIR/file${TESTFILE_SIZE_KB} -L ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/readtestfiles${TESTFILE_SIZE_KB}/testfile_${TESTFILE_SIZE_KB}_$k 1>>$output 2>>$error &
+        echo "curl -s -S -k --connect-timeout 10 --user $USER:$PASSWD -T $WRITEDIR/file${TESTFILE_SIZE_KB} -L ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/readtestfiles${TESTFILE_SIZE_KB}/testfile_${TESTFILE_SIZE_KB}_$k" >>$output
+        curl -s -S -k --connect-timeout 10 --user $USER:$PASSWD -T $WRITEDIR/file${TESTFILE_SIZE_KB} -L ${PROTOCOL}://${REMOTE_SERVER}/${STORAGE_PATH}/readtestfiles${TESTFILE_SIZE_KB}/testfile_${TESTFILE_SIZE_KB}_$k 1>>$output 2>>$error &
         i=`expr $i + 1`
         k=`expr $k + 1`
         usleep $USEC
